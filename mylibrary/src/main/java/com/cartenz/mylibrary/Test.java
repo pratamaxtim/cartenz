06b0bc20d8669ff3a4ba868e41aa433ea3cc8eba
Pratama Budiman <pratamax@gmail.com>
2018-02-23 08:11:54 +0700

    ok

---
+++ mylibrary/src/main/java/com/cartenz/mylibrary/Test.java
@@ -0,0 +1,13 @@
+package com.cartenz.mylibrary;
+
+import android.util.Log;
+
+/**
+ * Created by pratama on 2/22/2018.
+ */
+
+public class Test {
+    public static void test() {
+        Log.wtf("Test_", "WAKWAW");
+    }
+}
